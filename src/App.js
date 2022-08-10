14f88d2acd3466cb52d4f748ea6bc041e7e9365b
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-08-09 21:26:47 -0500

    Hard coding data in for style set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
 
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=9f2d9424cc5c534fbd0ee7a8d383b2c5`
+
+  return (
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Dallas</p>
+          </div>
+          <div className="temp">
+            <h1>60°F</h1>
+          </div>
+          <div className="description">
+            <p>Cloudy</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>65°F</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="wind">
+            12 MPH
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
