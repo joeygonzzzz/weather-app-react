119585486b830f346bd92277bf1e0dd5f7dac3c6
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-08-09 22:23:00 -0500

    Adjusted input color and fixed string for input

--- src/App.js
+++ src/App.js
@@ -2,13 +2,33 @@ import React, {useState} from 'react';
 import axios from 'axios';
 
 function App() {
+  const [data,setData] = useState({})
+  const [location, setLocation] = useState('')
 
-  
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=9f2d9424cc5c534fbd0ee7a8d383b2c5`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=9f2d9424cc5c534fbd0ee7a8d383b2c5`
+
+  const searchLocation = (event) => {
+    if(event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+        })
+        setLocation('')
+      }
+    }
+
 
   return (
     <div className="app">
+      <div className='search'>
+        <input 
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder='Enter Location'
+        type='text' />
+      </div>
       <div className="container">
         <div className="top">
           <div className="location">
